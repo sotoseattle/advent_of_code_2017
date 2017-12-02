cf73dfd0bc0ff37da62c46a6466163dac525a31a
Francisco Soto <franciscosoto@sotoseattle-mbp.local>
2017-12-02 09:10:25 -0800

    refactor

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,17 +1,16 @@
-def big_diff(chorizo):
+def big_diff(linea):
     '''Return the difference between the biggest and smallest int from a string of tab separated numbers'''
-    nums = [int(c) for c in chorizo.split('\t')]
+    nums = [int(c) for c in linea.split('\t')]
     return max(nums) - min(nums)
 
-def even_div(chorizo):
+def even_div(linea):
     '''Return the division between two numbers with mod 0 from a string of tab separated numbers'''
-    nums = sorted([int(c) for c in chorizo.split('\t')], reverse=True)
+    nums = sorted([int(c) for c in linea.split('\t')], reverse=True)
     zen = len(nums)
     sol = [nums[i]/nums[j] for i in range(0, zen) for j in range(i+1, zen) if nums[i]%nums[j] == 0]
     return sol[0]
 
 def checksumo(inputo, func):
-    '''Return the sum of applying a function to each line of input'''
     return sum([func(linea) for linea in inputo.splitlines()])
 
 def checksumo_1(inputo):
