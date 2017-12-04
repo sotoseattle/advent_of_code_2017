f673ce69b1443f1e84beb268e1ac186a2f2c3400
Francisco Soto <franciscosoto@sotoseattle-mbp.local>
2017-12-03 20:00:39 -0800

    day 3

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,31 +1,75 @@
+import numpy as np
+
 def onion_ring(target):
     sol = {'sid': 1, 'max': 1, 'len': 1}
     while sol['max'] < target:
         len = (sol['sid'] * 4) + 4
-        sol['sid'] += 2
-        sol['max'] += len
-        sol['len'] = len
+        sol['sid'] += 2             # side length of square layer
+        sol['max'] += len           # max value of the onion layer
+        sol['len'] = len            # total number of digits in layer
     return sol
 
-def onion_distance(target):
-    if target == 1:
-        return 0
+def build_spiral(n):
+    '''Ugly as hell, should be refactored or redone'''
+    data = onion_ring(n)
+    max_side = data['sid']
+    top_level = int((max_side - 1) / 2)
+    spiral = np.zeros((max_side, max_side))
+    spiral[top_level, top_level] = 1
+    
+    start_max = 2
+    for level in range(1, top_level+1):
+        data = onion_ring(start_max)
+        start_max = data['max'] + 1
 
-    # extract the square layer of numbers that include the target
-    data = onion_ring(target)
-    b = [x for x in range(data['max'], data['max']-data['len'], -1)]
+        z = data['sid']
+        layer = [x for x in range(data['max'], data['max']-data['len'], -1)]
+        layer.sort()
+        layer.insert(0, layer[-1])
+        sides = [layer[x:x+z] for x in range(0, len(layer), z-1)]
+        a = top_level - int((z-1)/2)
+        b = top_level + int((z-1)/2) + 1
+        sides[0].reverse()
+        spiral[a:b, b-1] = sides[0]
+        sides[1].reverse()
+        spiral[a, a:b] = sides[1]
+        spiral[a:b, a] = sides[2]
+        spiral[b-1, a:b] = sides[3]
+    
+    return spiral
 
-    # manipulate it to recreate each of the sides of the square
-    b.sort()
-    b.insert(0, b[-1])
-    for x in range(0, len(b), data['sid']-1):
-        side = b[x:x+data['sid']]
-        if target in side:
-            # level is the number of concentric layers away from 1
-            level = (data['sid'] - 1) / 2
-            midpoint = side[int((len(side)-1)/2)]
-            # dist is the distance to the midpoint of the side
-            dist = abs(target - midpoint)
-            return  dist + level
+def prune(i, length):
+    base = [-1, 0, 1]
+    if i < 1:
+        del(base[0])
+    if i >= length-1:
+        del(base[-1])
+    return base
 
-    raise 'Malfunction brrrp!'
+def adjacent(i, j, I, J):
+    return [[i+x, j+y] for x in prune(i, I) for y in prune(j, J)]
+
+def resolve_1(target):
+    sp = build_spiral(target)
+    i, j = np.argwhere(sp == target)[0]
+    mid = int((sp.shape[0] - 1) / 2)
+    return abs(i - mid) + abs(j - mid)
+
+def resolve_2(n):
+    MAX_DIGIT = 100
+    spiral = build_spiral(MAX_DIGIT)
+
+    side = spiral.shape[0]
+    sol = np.zeros((side, side))
+    mid = int((side - 1) / 2)
+    sol[mid, mid] = 1
+
+    for x in range(1, int(spiral.max()+1)):
+        i, j = np.argwhere(spiral == x)[0]
+        val = int(sum([sol[a,b] for a,b in adjacent(i, j, side, side)]))
+        if val > n:
+            return val
+        else:
+            sol[i, j] = val
+    return sol
+
--- ex3/test_ex3.py
+++ ex3/test_ex3.py
@@ -18,13 +18,26 @@ def test_layer_III():
     assert layer['max'] == 49
     assert layer['len'] == 24
 
-def test_onion_dist_I():
-    assert onion_distance(1) == 0
-    assert onion_distance(2) == 1
-    assert onion_distance(8) == 1
-    assert onion_distance(3) == 2
-    assert onion_distance(7) == 2
-    assert onion_distance(20) == 3
-    assert onion_distance(44) == 5
-    assert onion_distance(289326) == 419
+def test_builder():
+    sp = build_spiral(9) 
+    assert sp[0,0] == 5.0
+    assert sp[2,2] == 9.0
 
+def test_adj():
+    assert adjacent(0,0, 3, 3) == [[0, 0], [0, 1], [1, 0], [1, 1]]
+    assert adjacent(2,0, 3, 3) == [[1, 0], [1, 1], [2, 0], [2, 1]]
+
+def test_onion_dist():
+    assert resolve_1(1) == 0
+    assert resolve_1(2) == 1
+    assert resolve_1(8) == 1
+    assert resolve_1(3) == 2
+    assert resolve_1(7) == 2
+    assert resolve_1(20) == 3
+    assert resolve_1(44) == 5
+    assert resolve_1(289326) == 419
+
+def test_resolve():
+    assert resolve_2(9) == 10
+    assert resolve_2(750) == 806
+    assert resolve_2(289326) == 295229
