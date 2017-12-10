7f06724dae112f63f00bee2ff118f415f958ae04
Francisco Soto <franciscosoto@sotoseattle-mbp.local>
2017-12-10 09:46:11 -0800

    ex9

--- ex9/ex9.py
+++ ex9/ex9.py
@@ -1,2 +1,32 @@
-def something():
-    return True
+import re
+import sys
+sys.setrecursionlimit(10_000)
+
+def score(inputo, abierto=False, level=0, tot=0):
+    if len(inputo) == 0:
+        return tot
+
+    char = inputo[0]
+    inputo = inputo[1:]
+    if char == '}':
+        abierto = False
+        tot += level
+        level -= 1
+    elif char == '{':
+        level += 1
+        abierto = True
+
+    return score(inputo, abierto, level, tot)
+
+def cleanup(inputo):
+    inputo = re.sub(r'\!.', '', inputo)
+    garbage = sum([len(e) for listo in re.findall(r'<([^>]*)>', inputo) for e in listo])
+    inputo = re.sub(r'(<[^>]*>)', '', inputo)
+    inputo = re.sub(r'[^{}]', '', inputo)
+    return (inputo, garbage)
+
+def part_1(inputo):
+    return score(cleanup(inputo)[0])
+
+def part_2(inputo):
+    return cleanup(inputo)[1]
--- ex9/test_ex9.py
+++ ex9/test_ex9.py
@@ -1,4 +1,17 @@
 from ex9 import *
 
-def test_something():
-    assert True == True
+def test_score():
+    assert part_1('{}') == 1
+    assert part_1('{{{}}}') == 6 # score of 1 + 2 + 3 = 6.
+    assert part_1('{{},{}}') == 5 # score of 1 + 2 + 2 = 5.
+    assert part_1('{{{},{},{{}}}}') == 16 # score of 1 + 2 + 3 + 3 + 3 + 4 = 16.
+    assert part_1('{<a>,<a>,<a>,<a>}') == 1 # score of 1.
+    assert part_1('{{<ab>},{<ab>},{<ab>},{<ab>}}') == 9 # score of 1 + 2 + 2 + 2 + 2 = 9.
+    assert part_1('{{<!!>},{<!!>},{<!!>},{<!!>}}') == 9 # score of 1 + 2 + 2 + 2 + 2 = 9.
+    assert part_1('{{<a!>},{<a!>},{<a!>},{<ab>}}') == 3 # score of 1 + 2 = 3.
+
+def test_part_1():
+    assert part_1(open('inputo.txt', 'r').read() ) == 11089
+
+def test_part_2():
+    assert part_2(open('inputo.txt', 'r').read() ) == 5288
